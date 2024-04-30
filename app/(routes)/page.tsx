bf379d3cc0e1027c2d601a0c59d417eae0a16c9b
theanh1508 <85997901+0862551396@users.noreply.github.com>
2024-04-30 20:10:05 +0700

    setup

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+      <Button>Theanh</Button>
+    </div>
+  );
+};
+
+export default HomePage;
