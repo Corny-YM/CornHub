c1acaefdaf7369be09fb37e4024932c3892ce866
theanh1508 <85997901+0862551396@users.noreply.github.com>
2024-05-19 21:47:23 +0700

    fix: waiting server clerk works again

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,9 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
-import { auth } from "@clerk/nextjs/server";
 
 export default function Page() {
-  const test = auth();
-  console.log(test.userId);
-  return <SignIn />;
-  // return <div>theanh</div>;
+  return <SignIn path="/sign-in" />;
 }
--- app/(auth)/sign-in/test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const PageSignIn = () => {
-  return <div>PageSignIn</div>;
-};
-
-export default PageSignIn;
