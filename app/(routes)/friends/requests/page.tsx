f5108a3fe38b275726b9b74a9a3bf7d217b23215
theanh1508 <85997901+0862551396@users.noreply.github.com>
2024-05-23 23:09:30 +0700

    feat: action, UI for friends requests page

--- app/(routes)/friends/requests/page.tsx
+++ app/(routes)/friends/requests/page.tsx
@@ -1,9 +1,46 @@
-import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const FriendRequestsPage = () => {
+import { cn } from "@/lib/utils";
+import prisma from "@/lib/prisma";
+import EmptyData from "@/components/empty-data";
+import RequestSenderCard from "@/components/pages/friends/request-sender-card";
+
+const FriendRequestsPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) return redirect("/sign-in");
+
+  const friendRequests = await prisma.friendRequest.findMany({
+    include: { sender: true },
+    where: { receiver_id: userId },
+  });
+
+  const senders = friendRequests.map((item) => item.sender);
+
   return (
     <div className="flex-1 h-full px-8 pt-4">
       <div className="font-semibold text-lg mb-4">Lời mời kết bạn</div>
+
+      {!senders.length && <EmptyData />}
+
+      {!!senders.length && (
+        <div
+          className={cn(
+            "w-full grid gap-2 pb-4",
+            "grid-cols-1",
+            "md:grid-cols-2",
+            "lg:grid-cols-3",
+            "xl:grid-cols-4",
+            "min-[1440px]:grid-cols-5",
+            "2xl:grid-cols-6"
+          )}
+        >
+          {senders.map((sender) => (
+            <RequestSenderCard key={sender.id} data={sender} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
---
+++ components/pages/friends/request-sender-card.tsx
@@ -0,0 +1,123 @@
+"use client";
+
+import Image from "next/image";
+import toast from "react-hot-toast";
+import { User } from "@prisma/client";
+import { useAuth } from "@clerk/nextjs";
+import { useCallback, useMemo, useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+
+import { acceptFriendRequest, deniedFriendRequest } from "@/actions/user";
+import { Button } from "@/components/ui/button";
+import NoAvatar from "@/public/no-avatar.jpg";
+
+interface Props {
+  data: User;
+}
+
+const RequestSenderCard = ({ data }: Props) => {
+  const { userId } = useAuth();
+  const [senderData, setSenderData] = useState<User>(data);
+  const [status, setStatus] = useState<"accept" | "denied" | null>(null);
+
+  const {
+    mutate: mutateAcceptFriendRequest,
+    isPending: isPendingAcceptFriendRequest,
+  } = useMutation({
+    mutationKey: ["account", "unfriend", userId, senderData.id],
+    mutationFn: acceptFriendRequest,
+    onSuccess() {
+      setStatus("accept");
+      toast.success(
+        `Đã chấp nhận lời mời kết bạn của "${senderData.full_name}"`
+      );
+    },
+    onError() {
+      toast.error("Chấp nhận lời mời thất bại. Vui lòng thử lại sau");
+    },
+  });
+  const {
+    mutate: mutateDeniedFriendRequest,
+    isPending: isPendingDeniedFriendRequest,
+  } = useMutation({
+    mutationKey: ["account", "denied-request", userId, senderData.id],
+    mutationFn: deniedFriendRequest,
+    onSuccess() {
+      setStatus("denied");
+      toast.success(`Đã từ chối lời mời kết bạn của "${senderData.full_name}"`);
+    },
+    onError() {
+      toast.error("Từ chối lời mời thất bại. Vui lòng thử lại sau");
+    },
+  });
+
+  const handleAcceptFriendRequest = useCallback(() => {
+    if (!userId || !senderData) return;
+    mutateAcceptFriendRequest({ userId, friendId: senderData.id });
+  }, [userId, senderData]);
+
+  const handleDeniedFriendRequest = useCallback(() => {
+    if (!userId || !senderData) return;
+    mutateDeniedFriendRequest({ userId, friendId: senderData.id });
+  }, [userId, senderData]);
+
+  const contentStatus = useMemo(() => {
+    if (status === "accept") return "Đã chấp nhận yêu cầu";
+    return "Đã xóa yêu cầu";
+  }, [status]);
+
+  const contentActions = useMemo(
+    () => (
+      <>
+        <Button
+          className="w-full hover:bg-primary/50"
+          size="sm"
+          disabled={isPendingAcceptFriendRequest}
+          onClick={handleAcceptFriendRequest}
+        >
+          Xác nhận
+        </Button>
+        <Button
+          className="w-full hover:bg-destructive/20"
+          variant="outline"
+          size="sm"
+          disabled={isPendingDeniedFriendRequest}
+          onClick={handleDeniedFriendRequest}
+        >
+          Xóa
+        </Button>
+      </>
+    ),
+    [
+      isPendingAcceptFriendRequest,
+      isPendingDeniedFriendRequest,
+      handleAcceptFriendRequest,
+      handleDeniedFriendRequest,
+    ]
+  );
+
+  return (
+    <div className="w-full flex flex-col items-center justify-center overflow-hidden rounded-lg shadow dark:bg-neutral-800 bg-[#f0f2f5]">
+      <div className="flex justify-center items-center relative w-full h-auto aspect-square">
+        <Image
+          className="absolute w-full h-full"
+          src={senderData.avatar || NoAvatar}
+          alt={senderData.full_name || "avatar-friends"}
+          fill
+        />
+      </div>
+      <div className="w-full flex flex-col p-3 gap-y-1">
+        <div className="font-medium">{senderData.full_name}</div>
+        {!status ? (
+          contentActions
+        ) : (
+          <Button className="select-none" disabled variant="outline">
+            {contentStatus}
+          </Button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RequestSenderCard;
